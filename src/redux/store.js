e3e86326c60a69fbcea4b6e93d67818b700de2ba
Andrew Higton <andrewhigton@outlook.com>
2019-10-23 09:02:24 +0100

    cart icon added, and cart dropdown components added, implemented cart reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import logger from 'redux-logger';
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
-
+//how does this bit work? 
 const store = createStore(rootReducer, applyMiddleware(...middlewares)); 
 
 export default store;
